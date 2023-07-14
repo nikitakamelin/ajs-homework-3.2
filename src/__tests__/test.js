bf51bd6bfbfaebcf1663ae81342690600475f75a
NK Test <nkamelin@gmail.com>
2023-07-14 07:51:19 +0300

    hw done, appveyor ?

---
+++ src/__tests__/test.js
@@ -0,0 +1,15 @@
+import sorting from '../sort';
+
+test('sorting array', () => {
+  const result = sorting([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  expect(result).toStrictEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
---
+++ src/sort.js
@@ -0,0 +1,5 @@
+export default function sorting(array) {
+  return array.sort((prev, next) => next.health - prev.health);
+}
+
+// console.log(sort(data));
